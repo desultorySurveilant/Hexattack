a3aab2ba06db64f976ab2fac7f2075a6e845e599
desultorySurveilant <antonio.marques@sbcglobal.net>
2019-11-09 00:04:52 -0600

    very basic first time stuffs

--- web/main.dart
+++ web/main.dart
@@ -1,7 +1,30 @@
 import 'dart:html';
 
+
+TextAreaElement board = TextAreaElement();
 void main() {
   Element output = querySelector('#output');
-  ;
+  TextInputElement moves = TextInputElement();
+  output.append(moves);
+  output.append(board);
 
+  moves.onBlur.listen(handleMove);
+}
+
+String handleInput(Event e) => (e.target as InputElement).value;
+void handleMove(Event e){
+  List<String> input = handleInput(e).split(' ');
+  Pawn pawn = Pawn(int.parse(input[0]), int.parse(input[1]), board);
+  pawn.draw();
+}
+
+class Pawn{
+  int row;
+  int column;
+  TextAreaElement board;
+  Pawn(this.row, this.column, this.board);
+
+  void draw(){
+    board.text = "($row, $column)";
+  }
 }
