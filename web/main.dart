c2c58d0fb2cc3ebf3b3eaac3145567484eb041e4
desultorySurveilant <antonio.marques@sbcglobal.net>
2019-11-09 17:22:56 -0600

    now leaves behind 'trails'

--- web/main.dart
+++ web/main.dart
@@ -6,63 +6,90 @@ import 'dart:math';
 
 TextAreaElement board = TextAreaElement();
 CanvasElement canvas = CanvasElement()..width = 400..height=400;
-List<Pawn> pawnList = List();
-List<Tile> tileList = List();
-List<Mark> markList = List();
-Pawn selected = null;
+List<Pawn> pawnList = <Pawn>[];
+List<Tile> tileList = <Tile>[];
+List<Mark> markList = <Mark>[];
+List<Move> moveQueue = <Move>[];
+List<Mark> turnMarks = <Mark>[];
+Pawn selected;
 Mode mode = Mode.neutral;
 
 void main() {
-  Element output = querySelector('#output');
+  final Element output = querySelector('#output');
   output.append(canvas);
-  tileList.addAll(superHex(4));
-  tileList.forEach((t)=>t.draw());
-  pawnList.add(Pawn.coords(3, 4, [0, 51, 0])..draw());
-  pawnList.add(Pawn.coords(0, 0, [0, 51, 0])..draw());
-  pawnList.add(Pawn.coords(-2, -3, [0, 51, 0])..draw());
+  tileList.addAll(superHex(3));
+  for(final Tile tile in tileList){
+    tile.draw();
+  }
+  pawnList.add(Pawn.coords(3, 3, <int>[0, 51, 0])..draw());
+  pawnList.add(Pawn.coords(0, 0, <int>[0, 51, 0])..draw());
+  pawnList.add(Pawn.coords(-2, -3, <int>[0, 51, 0])..draw());
   canvas.onClick.listen(handleClick);
 
 }
 void handleClick(MouseEvent e){
   canvas.context2D.setFillColorRgb(255, 255, 255);
   canvas.context2D.fillRect(0, 0, canvas.height, canvas.width);
-  Point p = getEventPoint(e);
-  pawnList.forEach((e) {
-    e.color = e.baseColor;
-  });
+  final Point<num> p = getEventPoint(e);
+  for(final Pawn p in pawnList){
+    p.color = p.baseColor;
+  }
   markList.clear();
   if(mode == Mode.neutral){
     handleNeutralClick(p);
   }else if(mode == Mode.moving){
     handleMoveClick(p);
   }
-  tileList.forEach((t)=>t.draw());
-  markList.forEach((m) => m.draw());
-  pawnList.forEach((p) => p.draw());
+  for(final Tile tile in tileList){
+    tile.draw();
+  }
+  for(final Mark mark in markList){
+    mark.draw();
+  }
+  for(final Pawn pawn in pawnList){
+    pawn.draw();
+  }
+  for(final Move move in moveQueue){
+    move.draw();
+  }
 }
-void handleNeutralClick(Point p){
-  Pawn target = pawnList.firstWhere((w) => w.tile.pointInHex(p), orElse: ()=>null);
+void handleNeutralClick(Point<num> p){
+//  moveQueue.clear();
+  final Pawn target = pawnList.firstWhere((Pawn w) => w.tile.pointInHex(p), orElse: ()=>null);
   if(target != null){
-    target.color = [255, 0, 0];
+//    target.color = [255, 0, 0];
     target.draw();
-    Iterable<Tile> adj = getAdjacents(target.tile);
-    markList.addAll(adj.map((t) => Mark(t, [255, 255, 0])));
+    final Iterable<Tile> adj = getAdjacents(target.tile);
+    markList.addAll(adj.map((Tile t) => Mark(t, <int>[255, 255, 0])));
     selected = target;
     mode = Mode.moving;
   }
 }
-void handleMoveClick(Point p){
-  Tile target = tileList.firstWhere((w) => w.pointInHex(p), orElse: ()=>null);
+void handleMoveClick(Point<num> p){
+  final Tile target = tileList.firstWhere((Tile w) => w.pointInHex(p), orElse: ()=>null);
   if(target != null){
     selected.color = selected.baseColor;
-    Iterable<Tile> adj = getAdjacents(selected.tile);
-    if (adj.contains(target) && !pawnList.any((w)=> w.tile == target)){
+    final Iterable<Tile> adj = getAdjacents(selected.tile);
+    if (adj.contains(target) && !pawnList.any((Pawn w)=> w.tile == target)){
+      moveQueue.add(MoveMove(selected.tile, relativeDirection(selected.tile, target)));
       selected.tile = target;
     }
   }
+  selected = null;
   mode = Mode.neutral;
 }
-Iterable<Tile> getAdjacents(Tile target) => tileList.where((t){
+Direction relativeDirection(Tile origin, Tile target){
+  final int rDelta = origin.row - target.row;
+  final int cDelta = origin.column - target.column;
+  if(cDelta == -1){
+    return rDelta == -1 ? Direction.southeast : Direction.northeast;
+  }else if(cDelta == 0){
+    return rDelta == -1 ? Direction.south : Direction.north;
+  }else{
+    return rDelta == 0 ? Direction.southwest : Direction.northwest;
+  }
+}
+Iterable<Tile> getAdjacents(Tile target) => tileList.where((Tile t){
   final int rDelta = target.row - t.row;
   final int cDelta = target.column - t.column;
   if(rDelta.abs() + cDelta.abs() == 1){
@@ -74,7 +101,7 @@ Iterable<Tile> getAdjacents(Tile target) => tileList.where((t){
   }
 });
 List<Tile> superHex(int n){
-  List<Tile> ret = List();
+  final List<Tile> ret = <Tile>[];
   for(int i = -n; i <= n; i++){
     for(int j = max(-n, -n + i); j <= min(n, n + i); j++){
       ret.add(Tile(i, j, canvas));
@@ -86,17 +113,16 @@ List<Tile> superHex(int n){
 class Tile{
   static const num radius = 24;
   static const num innerScale = 3/4;
-  static const xOffset = 200;
-  static const yOffset = 200;
+  static const int xOffset = 200;
+  static const int yOffset = 200;
 
   final int row;
   final int column;
   final CanvasElement board;
   Tile(this.row, this.column, this.board);
-  Point p = Point(0,0);
 
   void draw([List<int> color]){
-    bool willColor = color != null;
+    final bool willColor = color != null;
     if(willColor){
       this.board.context2D.setFillColorRgb(color[0], color[1], color[2]);
     }
@@ -105,16 +131,19 @@ class Tile{
     drawSpokes(centerX, centerY, radius * innerScale, radius, canvas);
 
   }
-  bool pointInHex(Point p){
-    final Point rp = Point((p.x - centerX).abs(), (p.y - centerY).abs());
-    bool vertTest = rp.x < radius * sqrt(3) / 2;
-    bool slopeTest = rp.y < radius - rp.x / 2;
+  void drawEdge(List<int> color, Direction direction){
+    drawTrapezoid(centerX, centerY, direction, radius * innerScale, radius, canvas, color);
+  }
+  bool pointInHex(Point<num> p){
+    final Point<num> rp = Point<num>((p.x - centerX).abs(), (p.y - centerY).abs());
+    final bool vertTest = rp.x < radius * sqrt(3) / 2;
+    final bool slopeTest = rp.y < radius - rp.x / 2;
     return vertTest && slopeTest;
   }
   num get centerX => column * radius * 3/2 + xOffset;
   num get centerY => row * radius * sqrt(3) - column * radius * sqrt(3) / 2 + yOffset;
 }
-Tile tileByCoords(int r, int c) => tileList.firstWhere((t)=>
+Tile tileByCoords(int r, int c) => tileList.firstWhere((Tile t)=>
   t.row == r && t.column == c
 , orElse: ()=>null);
 
@@ -129,20 +158,20 @@ class Pawn{
     color = baseColor;
     tile = tileByCoords(r, c);
   }
-  draw(){
+  void draw(){
     tile.draw(color);
   }
 }
 class Mark{
   final List<int> color;
-  final tile;
+  final Tile tile;
   Mark(this.tile, this.color);
-  draw(){
+  void draw(){
     tile.draw(color);
   }
 }
-void drawHex(num x, num y, num r, CanvasElement canvas, [fill = false]){
-  CanvasRenderingContext2D ctx = canvas.context2D;
+void drawHex(num x, num y, num r, CanvasElement canvas, [bool fill = false]){
+  final CanvasRenderingContext2D ctx = canvas.context2D;
   ctx.beginPath();
   ctx.moveTo(x + r, y);
   for(int i = 1; i <= 6; i++){
@@ -154,7 +183,7 @@ void drawHex(num x, num y, num r, CanvasElement canvas, [fill = false]){
   ctx.stroke();
 }
 void drawSpokes(num x, num y, num r1, num r2, CanvasElement canvas){
-  CanvasRenderingContext2D ctx = canvas.context2D;
+  final CanvasRenderingContext2D ctx = canvas.context2D;
   ctx.beginPath();
   for(int i = 0; i < 6; i++){
     ctx.moveTo(x + r1*cos(i * pi/3), y + r1*sin(i * pi/3));
@@ -163,12 +192,86 @@ void drawSpokes(num x, num y, num r1, num r2, CanvasElement canvas){
   ctx.stroke();
 }
 
-Point getEventPoint(MouseEvent e){
-  CanvasElement target = e.target;
-  Rectangle rect = target.getBoundingClientRect();
-  return Point(e.client.x-rect.left, e.client.y-rect.top);
+void drawTrapezoid(num centerX, num centerY, Direction direction,num r1, num r2, CanvasElement canvas, List<int> color) {
+  final CanvasRenderingContext2D ctx = canvas.context2D;
+  ctx.setFillColorRgb(color[0], color[1], color[2]);
+  final double rotation = directionToRotation(direction);
+  final double firstX = centerX + r1 * cos(rotation + pi/6);
+  final double firstY = centerY + r1 * sin(rotation + pi/6);
+  final double secondX = centerX + r2 * cos(rotation + pi/6);
+  final double secondY = centerY + r2 * sin(rotation + pi/6);
+  final double thirdX = centerX + r1 * cos(rotation - pi/6);
+  final double thirdY = centerY + r1 * sin(rotation - pi/6);
+  final double fourthX = centerX + r2 * cos(rotation - pi/6);
+  final double fourthY = centerY + r2 * sin(rotation - pi/6);
+  ctx.beginPath();
+  ctx.moveTo(firstX, firstY);
+  ctx.lineTo(secondX, secondY);
+  ctx.lineTo(fourthX, fourthY);
+  ctx.lineTo(thirdX, thirdY);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+}
+Point<num> getEventPoint(MouseEvent e){
+  final CanvasElement target = e.target;
+  final Rectangle<num> rect = target.getBoundingClientRect();
+  return Point<num>(e.client.x-rect.left, e.client.y-rect.top);
 }
 enum Mode{
   neutral,
   moving
+}
+enum Direction{
+  northeast,
+  north,
+  northwest,
+  southwest,
+  south,
+  southeast
+}
+abstract class Move{
+  final Tile source;
+  final Direction direction;
+  Move(this.source, this.direction);
+  void draw();
+  void call();
+}
+class MoveMove extends Move{
+  MoveMove(Tile e, Direction d):super(e, d);
+  @override
+  void draw(){
+    for(final Direction d in Direction.values){
+      drawTrapezoid(source.centerX, source.centerY, d, Tile.radius * Tile.innerScale, Tile.radius, canvas, <int>[0, 51, 0]);
+    }
+    drawTrapezoid(source.centerX, source.centerY, direction, Tile.radius * Tile.innerScale, Tile.radius, canvas, <int>[255, 255, 0]);
+  }
+  @override
+  void call() {
+    // TODO: implement call
+  }
+}
+double directionToRotation(Direction direction) {
+  switch (direction) {
+    case Direction.southeast:
+      return pi / 6;
+      break;
+    case Direction.south:
+      return pi/2;
+      break;
+    case Direction.southwest:
+      return 5 * pi / 6;
+      break;
+    case Direction.northwest:
+      return 7 * pi / 6;
+      break;
+    case Direction.north:
+      return 3 * pi / 2;
+      break;
+    case Direction.northeast:
+      return 11 * pi / 6;
+      break;
+    default:
+      throw(Error());
+  }
 }
